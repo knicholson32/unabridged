54c17cc8d8c80eefb99de69828083389793305da
knicholson32 <keenanrnicholson@gmail.com>
2022-11-19 08:53:46 -0500

    Changed the home folder for vite to match that of the beets server default (/config).

--- ui/src/routes/api/add/+server.ts
+++ ui/src/routes/api/add/+server.ts
@@ -2,6 +2,11 @@ import { json } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 import * as db from '$lib/server/db';
 import type { LibraryItemBookInfo } from '$lib/server/db';
+import {Exec} from '$lib/server/utils'
+import os from 'os';
+
+// check the available memory
+const userHomeDir = os.homedir();
 
 export const POST: RequestHandler = async ({ request }) => {
     const { a, b } = await request.json();
@@ -10,5 +15,7 @@ export const POST: RequestHandler = async ({ request }) => {
     //   arn: 'test123!'
     // } as LibraryItemBookInfo);
     // db.write();
+    console.log(await(await (new Exec('pwd').execute())).stdout);
+    console.log(userHomeDir);
     return json(a + b);
 };
