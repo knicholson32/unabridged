d9deb9087c6efae11a3f677c4714f295fecf1ee1
Keenan Nicholson <keenanrnicholson@gmail.com>
2023-11-22 18:34:06 -0500

    Fixed a bug where book titles would include extra information such as series info.

--- app/src/lib/server/cmd/AAXtoMP3/cmd/convert/index.ts
+++ app/src/lib/server/cmd/AAXtoMP3/cmd/convert/index.ts
@@ -91,7 +91,7 @@ export const exec = async (asin: string, processID: string, tmpDir: string): Pro
   // AAXtoMP3 -e:m4b -s --author "Martha Wells" --authcode 4165af03 --dir-naming-scheme '$artist/$title' --file-naming-scheme '$title' --use-audible-cli-data -t /app/db/export ./*.aaxc
   // '--debug', `--audible-cli-library-file`, `"/db/audible/${profileID}.library.tsv"`
   // TODO: Save the directory and filename so we can just save them to the DB to make deleting books easier
-  const args = ['-e:m4b', `-L`, `/db/audible/${profileID}.library.tsv`, '-s', '--author', `"${book.authors[0].name}"`, '--authcode', authCode, '--dir-naming-scheme', `'"${sanitizeFile(book.authors[0].name)}/${sanitizeFile(book.title)}"'`, '--file-naming-scheme', `'"${sanitizeFile(book.title)}"'`, '--use-audible-cli-data', '-t', LIBRARY_FOLDER, './*.aaxc']
+  const args = ['-e:m4b', `-L`, `/db/audible/${profileID}.library.tsv`, '-s', '--author', `"${book.authors[0].name}"`, '--title', `"${book.title}"`, '--authcode', authCode, '--dir-naming-scheme', `'"${sanitizeFile(book.authors[0].name)}/${sanitizeFile(book.title)}"'`, '--file-naming-scheme', `'"${sanitizeFile(book.title)}"'`, '--use-audible-cli-data', '-t', LIBRARY_FOLDER, './*.aaxc']
   // Check if we should use debug mode
   if (await settings.get('system.debug') === true) args.unshift('--debug');
   console.log(`${AAXtoMP3_COMMAND} ${args.join(' ')}`);
