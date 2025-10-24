44dc1bf5a267cdaf5aa08c325b2f0991084668a5
Keenan Nicholson <keenanrnicholson@gmail.com>
2025-10-23 21:53:42 -0400

    Fixed a bug where Unabridged would see books as recently added even when they were just synced from source.

--- src/lib/server/cmd/audible/cmd/library/index.ts
+++ src/lib/server/cmd/audible/cmd/library/index.ts
@@ -288,7 +288,7 @@ const processBook = async (book: BookFromCLI, id: string): Promise<boolean> => {
 				purchase_date: purchase_date,
 				downloaded: false,
 				processed: false,
-				date_added: Math.floor(new Date().getTime() / 1000),
+				date_added: 0,
 				cover: {
 					create: {
 						url_50: cover_url_50,
